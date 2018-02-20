e04b1b9166c1fdf8d8ac266aae65a60921a1f600
Wagner Almeida <wdmeida@gmail.com>
2018-02-20 15:15:14 -0300

    Calc lib

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,62 @@
-const { expect } = require('chai');
+import { expect } from 'chai';
+import { sum, sub, mult, div } from '../src/main';
 
 describe('Main', () => {
+  describe('Smoke tests', () => {
+    // it('should exist the calc lib', () => {
+    //   expect(calc).to.exist;
+    // });
 
+    it("should exist the method 'sum'", () => {
+      expect(sum).to.exist;
+      expect(sum).to.be.a('function');
+    });
+
+    it("should exist the method 'sub'", () => {
+      expect(sub).to.exist;
+      expect(sub).to.be.a('function');
+    });
+
+    it("should exist the method 'mult'", () => {
+      expect(mult).to.exist;
+      expect(mult).to.be.a('function');
+    });
+
+    it("should exist the method 'div'", () => {
+      expect(div).to.exist;
+      expect(div).to.be.a('function');
+    });
+  });
+
+  describe('Sum', () => {
+    it("should return 4 when 'sun(2,2)'", () => {
+      expect(sum(2, 2)).to.be.equal(4);
+    });
+  });
+
+  describe('Sub', () => {
+    it("should return 4 when 'sub(6,2)'", () => {
+      expect(sub(6, 2)).to.equal(4);
+    });
+
+    it("should return -4 when 'sub(6,10)'", () => {
+      expect(sub(6, 10)).to.equal(-4);
+    });
+  });
+
+  describe('Mult', () => {
+    it("should return 4 when 'mult(2, 2)'", () => {
+      expect(mult(2, 2)).to.equal(4);
+    });
+  });
+
+  describe('Div', () => {
+    it("should return 2 when 'div(4,2)'", () => {
+      expect(div(4, 2)).to.equal(2);
+    });
+
+    it("should return 'Não é possível divisão por zero!' when divide by 0", () => {
+      expect(div(4, 0)).to.equal('Não é possível divisão por zero!');
+    });
+  });
 });
