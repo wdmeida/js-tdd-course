c2fc7a70448d746704d481b0209653be90c620ae
Wagner Almeida <wdmeida@gmail.com>
2018-02-20 15:34:41 -0300

    Add spec test

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,18 @@
+/*
+  Desafio FizzBuzz
+
+  Escreva uma lib que recebe um número e:
+
+  Se o número for divisível por 3, no logar do número escreva 'Fizz'
+  Se o número for divisível por 5, no logar do número escreva 'Buzz'
+  Se o número for divisível por 3 e 5, no logar do número escreva 'FizzBuzz'
+  Se não for múltiplo de nada, retorne o número.
+*/
+
 import { expect } from 'chai';
 
 describe('Main', () => {
-
+  it("should return 'Fizz' when multiple of 3", () => {
+    expect(FizzBuzz(3)).to.equal('Fizz');
+  });
 });
